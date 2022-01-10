227a0fa81e8b09a4f675164ed12ef1738a080bfc
Muhammad Rizky Nur Fajrie <86530834+mrizkynurfajrie@users.noreply.github.com>
2022-01-10 09:35:07 +0800

    membuat file controller

--- rest.js
+++ rest.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const conn = require("./koneksi");
+
 exports.ok = function(values, res){
     var data = {
         'status':200,
         'values':values
     };
-    
+
      res.json(data);
      res.end();
 }
