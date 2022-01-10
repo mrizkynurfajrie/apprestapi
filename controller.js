e056bbe806c63cbc1e5b9395ff40dfe8a12d0995
Muhammad Rizky Nur Fajrie <86530834+mrizkynurfajrie@users.noreply.github.com>
2022-01-10 10:17:06 +0800

    menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -2,19 +2,33 @@
 
 var response = require('./rest');
 var connection = require('./koneksi');
-const { connect } = require('./koneksi');
 
-exports.index = function(req,res){
-    response.ok("Aplikasi Rest API ku Berjalan!",res)
+
+exports.index = function (req, res) {
+    response.ok("Aplikasi Rest API ku Berjalan!", res)
 }
 
 //menampilkan semua data mahasiswa
-exports.tampilsemuadata = function(req, res){
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
-        if(error){
-            connection.log(error);
-        }else{
+exports.tampilsemuadata = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
             response.ok(rows, res)
         }
     });
+};
+
+//menampilkan semua data mahasiswa berdasarkan ID
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res)
+            }
+        }
+    );
 };
